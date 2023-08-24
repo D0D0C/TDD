069b21f1c95a211184c2b67b4c15366c6610989e
Dorian_CUVELIER <cuvelierdorian@gmail.com>
2023-08-24 14:01:33 +0200

    MAJ FiBTest

--- src/test/java/org/example/FibTest.java
+++ src/test/java/org/example/FibTest.java
@@ -1,2 +1,33 @@
-package org.example;public class FibTest {
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+public class FibTest {
+
+    @Test
+    public void testFibSeriesWithRange1() {
+        Fib fib = new Fib(1);
+        List<Integer> result = fib.getFibSeries();
+
+        assertFalse(result.isEmpty(), "non vide");
+        assertEquals(List.of(0), result, "Liste de 0 ");
+    }
+
+    @Test
+    public void testFibSeriesWithRange6() {
+        Fib fib = new Fib(6);
+        List<Integer> result = fib.getFibSeries();
+
+        assertTrue(result.contains(3), "Contient le chiffre 3");
+        assertEquals(6, result.size(), "Contient 6 éléments"); //
+        assertFalse(result.contains(4), "Pas de chiffre 4 ");
+        assertEquals(List.of(0, 1, 1, 2, 3, 5), result, "Liste qui contient {0, 1, 1, 2, 3, 5}");
+
+
+        for (int i = 1; i < result.size(); i++) {
+            assertTrue(result.get(i - 1) <= result.get(i), "Le résultat ascendant");
+        }
+    }
 }
--- src/test/java/org/example/GradingCalculatorTest.java
+++ src/test/java/org/example/GradingCalculatorTest.java
@@ -1,2 +1,63 @@
-package org.example;public class GradingCalculatorTest {
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GradingCalculatorTest {
+
+    @Test
+    public void testGradeA() {
+        GradingCalculator calculator = new GradingCalculator();
+        calculator.setScore(95);
+        calculator.setAttendancePercentage(90);
+        assertEquals('A', calculator.getGrade());
+    }
+
+    @Test
+    public void testGradeB1() {
+        GradingCalculator calculator = new GradingCalculator();
+        calculator.setScore(85);
+        calculator.setAttendancePercentage(90);
+        assertEquals('B', calculator.getGrade());
+    }
+
+    @Test
+    public void testGradeC() {
+        GradingCalculator calculator = new GradingCalculator();
+        calculator.setScore(65);
+        calculator.setAttendancePercentage(90);
+        assertEquals('C', calculator.getGrade());
+    }
+
+    @Test
+    public void testGradeB2() {
+        GradingCalculator calculator = new GradingCalculator();
+        calculator.setScore(95);
+        calculator.setAttendancePercentage(65);
+        assertEquals('B', calculator.getGrade());
+    }
+
+    @Test
+    public void testGradeF1() {
+        GradingCalculator calculator = new GradingCalculator();
+        calculator.setScore(95);
+        calculator.setAttendancePercentage(55);
+        assertEquals('F', calculator.getGrade());
+    }
+
+    @Test
+    public void testGradeF2() {
+        GradingCalculator calculator = new GradingCalculator();
+        calculator.setScore(65);
+        calculator.setAttendancePercentage(55);
+        assertEquals('F', calculator.getGrade());
+    }
+
+    @Test
+    public void testGradeF3() {
+        GradingCalculator calculator = new GradingCalculator();
+        calculator.setScore(50);
+        calculator.setAttendancePercentage(90);
+        assertEquals('F', calculator.getGrade());
+    }
 }
